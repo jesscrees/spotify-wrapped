8958d188597de7e7297e50eea41440338c0b008b
Jess Crees <jessicacrees@gmail.com>
2023-10-14 09:34:24 +0100

    Updated next config to be more relaxed about image hostnames

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'scontent-ams4-1.xx.fbcdn.net',
+        hostname: '*.fbcdn.net',
       },
       {
         protocol: 'https',
